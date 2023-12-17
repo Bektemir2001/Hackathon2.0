829f52e5903225ab805ea7880b2183bcd0cb2d76
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2023-12-17 10:42:28 +0600

    report

--- app/Export/EventReportExport.php
+++ app/Export/EventReportExport.php
@@ -2,7 +2,39 @@
 
 namespace App\Export;
 
-class EventReportExport
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Support\Collection;
+class EventReportExport implements FromCollection
 {
+    protected mixed $data;
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
 
+    public function collection()
+    {
+
+        $collection = collect([$this->getHeaders()]);
+
+        foreach ($this->data as $item) {
+            $row = []; // Создаем новую строку для каждой записи
+
+            array_push($row, $item->name);
+            array_push($row, $item->surname);
+            array_push($row, $item->event_name);
+            array_push($row, $item->date_event);
+            array_push($row, $item->status);
+
+            $collection->push($row); // Добавляем строку в коллекцию
+        }
+
+        return $collection;
+    }
+
+    private function getHeaders()
+    {
+        // Возвращаем массив с заголовками столбцов
+        return ['Имя', 'Фамилия', 'Меропрятие', 'Дата меропрятие', 'статус']; // Замените на свои заголовки
+    }
 }
--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -2,18 +2,25 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Export\EventReportExport;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ReportController extends Controller
 {
     public function report(int $event_id)
     {
 
-        $event = DB::table('event')
-        ->where('id', $event_id)->first();
-
-        return response(['data' => $event]);
+        $data = DB::table('event')
+            ->join('event_participants_attendance as p', 'p.event_id', '=', 'event.id')
+            ->join('employee as e', 'e.id', '=', 'p.participants_attendance_key')
+            ->select('e.firstname as name', 'e.lastname as surname', 'event.start_date_time as date_event',
+                DB::raw('CASE WHEN p.participants_attendance = 1 THEN "пришел" ELSE "не пришел" END as status'),
+            'event.name as event_name')
+            ->where('event.id', $event_id)->get();
+        $event = DB::table('event')->where('event.id', $event_id)->first();
+        return Excel::download(new EventReportExport($data), $event->name.'.xlsx');
     }
 }
